5ff49cbc2b66d4372bfb822df87f781dcd7e8f58
Adebimpe Adetomiwa <adebimpeadetomiwa22@gmail.com>
2024-12-31 12:49:05 -0800

    add extra security packages

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 require('express-async-errors');
 const connectDB = require('./db/connect');
 require('dotenv').config();
-const cors = require('cors');
+// const cors = require('cors');
 const port = process.env.PORT || 5000;
 const booksRouter = require('./routes/books');
 const userRouter = require('./routes/users');
@@ -11,8 +11,17 @@ const notFound = require('./middlewares/notFound');
 const errorHandler = require('./middlewares/errorHandler');
 // 673652a8376722ee86fc8abc
 
-app.use(cors());
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+// app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the homepage');
